61bd5cbab195b09a52ac256a036a15b76e4902ca
Maks3w <github.maks3w@virtualplanets.net>
2013-10-02 08:55:48 +0200

    Replace original GitHub merge form with GHGFK Merge Button (ntm)

--- extension/inject.js
+++ extension/inject.js
@@ -1,32 +1,35 @@
-var insertPoint = $('.merge-branch-form-actions');
+var angularBaseElement = $('#js-pull-merging');
+var insertPoint = $('.merge-form-contents');
 
-angular.element(insertPoint).ready(function () {
-  console.log('GHGFK: Start');
+if (angularBaseElement.length !== 0) {
+  angular.element(angularBaseElement).ready(function () {
+    console.log('GHGFK: Start');
 
-  var $injector = angular.bootstrap(insertPoint, ['GHGFK.controllers', 'maks3w.github', 'maks3w.github.directives']);
+    var $injector = angular.bootstrap(angularBaseElement, ['GHGFK.controllers', 'maks3w.github', 'maks3w.github.directives']);
 
-  chrome.storage.sync.get('githubToken', function (options) {
-    console.log('GHGFK: Retriving GitHub token');
+    chrome.storage.sync.get('githubToken', function (options) {
+      console.log('GHGFK: Retriving GitHub token');
 
-    $injector.invoke(function ($githubConfigurator) {
-      console.log('GHGFK: Setting GitHub token');
-      $githubConfigurator.token = options.githubToken;
-    });
+      $injector.invoke(function ($githubConfigurator) {
+        console.log('GHGFK: Setting GitHub token');
+        $githubConfigurator.token = options.githubToken;
+      });
 
-    $injector.invoke(function ($github, $rootScope, $compile) {
-      var $scope = angular.element(insertPoint).scope();
+      $injector.invoke(function ($github, $rootScope, $compile) {
+        var $scope = angular.element(angularBaseElement).scope();
 
-      var regexp = new RegExp(/^\/(.*)\/pull\/(\d+)/)
-      var routeParams = regexp.exec(location.pathname);
-      var repoFullName = routeParams[1];
-      var prNumber = routeParams[2];
+        var regexp = new RegExp(/^\/(.*)\/pull\/(\d+)/)
+        var routeParams = regexp.exec(location.pathname);
+        var repoFullName = routeParams[1];
+        var prNumber = routeParams[2];
 
-      console.log('GHGFK: Pull request');
-      $github.one('repos', repoFullName).one('pulls', prNumber).get().then(function (pr) {
-        $scope.pr = pr;
+        console.log('GHGFK: Pull request');
+        $github.one('repos', repoFullName).one('pulls', prNumber).get().then(function (pr) {
+          $scope.pr = pr;
 
-        insertPoint.append($compile('<merge-button pr="pr" />')($scope));
+          insertPoint.html($compile('<merge-button pr="pr" />')($scope));
+        });
       });
     });
   });
-});
+}

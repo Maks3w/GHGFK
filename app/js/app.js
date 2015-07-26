2a2be003a162c742c8180dc1036a9f553b98a4d2
Maks3w <github.maks3w@virtualplanets.net>
2015-07-26 10:24:56 +0200

    Remove angular-bootstrap

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('GHGFK', ['maks3w.github', 'maks3w.github.directives', 'ui.bootstrap', 'ngRoute']).
+angular.module('GHGFK', ['maks3w.github', 'maks3w.github.directives', 'ngRoute']).
   config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {redirectTo : '/repo'});
     $routeProvider.when('/auto_login', {templateUrl: 'partials/autoLogin.html', controller: 'AutoLoginController'});

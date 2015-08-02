f545b56931c08dd197979e4b8858fb34e66410a4
Maks3w <github.maks3w@virtualplanets.net>
2015-08-02 20:01:26 +0200

    [extension] Fix repoFactory is not a function

--- extension/content/app.ts
+++ extension/content/app.ts
@@ -1,5 +1,6 @@
 import {default as GithubModule }  from "../../app/lib/github/module.ts";
 import {default as GithubDirectives }  from "../../app/lib/github/Directive/module.ts";
+import {RepositoryFactory} from "../../app/lib/github/RepositoryService.ts";
 
 let angularBaseElement = document.getElementsByClassName("js-pull-merging").item(0);
 let insertPointElement = document.getElementsByClassName("merge-form-contents").item(0);
@@ -22,18 +23,19 @@ if (angularBaseElement && insertPointElement) {
             $githubConfigurator.token = options.githubToken;
         }]);
 
-        $injector.invoke(["github.repository", "$rootScope", "$compile", function (repoFactory, $rootScope, $compile) {
-            let routeParams = new RegExp(/^\/(.*)\/pull\/(\d+)/).exec(location.pathname);
-            let repoFullName = routeParams[1];
-            let prNumber = routeParams[2];
+        $injector.invoke(["github.repository", "$compile",
+            function (repoFactory:RepositoryFactory, $compile:ng.ICompileService) {
+                let routeParams = new RegExp(/^\/(.*)\/pull\/(\d+)/).exec(location.pathname);
+                let repoFullName = routeParams[1];
+                let prNumber = routeParams[2];
 
-            console.log(`GHGFK: Pull request #${prNumber}`);
-            repoFactory(repoFullName).getPr(prNumber).then(function (pr) {
-                let $scope = insertPointElement.scope();
-                $scope.pr = pr;
+                console.log(`GHGFK: Pull request #${prNumber}`);
+                repoFactory.repository(repoFullName).getPr(prNumber).then(function (pr) {
+                    let $scope = insertPointElement.scope();
+                    $scope.pr = pr;
 
-                insertPointElement.html($compile('<merge-button pr="pr" />')($scope));
-            });
-        }]);
+                    insertPointElement.html($compile('<merge-button pr="pr" />')($scope));
+                });
+            }]);
     });
 }
